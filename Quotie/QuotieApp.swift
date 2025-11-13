3458f3b064519c1dc74875715be9f5a92b288470
Andre <abradfordpoetry@gmail.com>
2025-11-13 17:35:00 -0600

    Initial Commit

---
+++ Quotie/QuotieApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotieApp.swift
+//  Quotie
+//
+//  Created by Andre Bradford on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct QuotieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
